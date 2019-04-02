4f2c946ce5f3c698d53f9e178f890023f1295062
bilquist <bbilquist@gmail.com>
2019-04-02 08:53:20 -0700

    updated functional tests and allowed_hosts

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,9 +1,30 @@
 # tests/functional_tests.py
 
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_create_a_profile(self):
+		# Alice has heard about a cool new online dnd app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		# She notices the page title and header mention 'Drage Eventyr'
+		self.assertIn('Critical Skol', self.browser.title)
+
+		# Auto fail
+		self.fail('Finish the test!')
+		
+		# She is invited to click to create an account
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
