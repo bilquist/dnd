d429542db0ee57537d7440c7acd09a3fcce5a5b2
bilquist <bbilquist@gmail.com>
2019-04-02 13:45:50 -0700

    Model for participants and associated tests

--- initiative/models.py
+++ initiative/models.py
@@ -1,3 +1,8 @@
+# initiative/models.py
+
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+	name = models.TextField()
+	is_pc = models.BooleanField(default=False)
--- initiative/tests.py
+++ initiative/tests.py
@@ -3,6 +3,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from initiative.models import Participant
 
 from initiative.views import home_page
 
@@ -19,3 +20,29 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'participant_text': 'A new participant'})
 		self.assertIn('A new participant', response.content.decode())
 		self.assertTemplateUsed(response, 'initiative/home.html')
+
+
+class ParticipantModelTest(TestCase):
+	
+	def test_saving_and_retrieving_participants(self):
+		first_participant = Participant()
+		first_participant.name = 'Alice'
+		first_participant.is_pc = 1
+		first_participant.save()
+		
+		second_participant = Participant()
+		second_participant.name = 'Bob'
+		second_participant.is_pc = 1
+		second_participant.save()
+		
+		saved_participants = Participant.objects.all()
+		self.assertEqual(saved_participants.count(), 2)
+		
+		first_saved_participant = saved_participants[0]
+		second_saved_participant = saved_participants[1]
+		self.assertEqual(first_saved_participant.name, 'Alice')
+		self.assertEqual(first_saved_participant.is_pc, 1)
+		self.assertEqual(second_saved_participant.name, 'Bob')
+		
+		
+		
