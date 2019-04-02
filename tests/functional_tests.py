b80cde83c0c5a3fe0c3ca9dd63bea952666be53c
bilquist <bbilquist@gmail.com>
2019-04-02 10:42:24 -0700

    Functional test now checks we can enter a player character

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,8 @@
 # tests/functional_tests.py
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -13,13 +15,39 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 	
-	def test_can_create_a_profile(self):
+	def test_can_start_an_initiative_and_retrieve_it_later(self):
 		# Alice has heard about a cool new online dnd app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
 
-		# She notices the page title and header mention 'Drage Eventyr'
+		# She notices the page title and header mention 'Initiative'
 		self.assertIn('Initiative', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1')
+		self.assertIn('Participants', headder_text)
+		
+		# She is invited to enter a participant right away
+		inputbox = self.browser.find_element_by_id('id_new_participant')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a participant'
+		)
+		
+		# She types "Player Character 1" into a text box (Alice is entering a PC)
+		inputbox.send_keys('Player Character 1')
+		
+		# When she hits enter, the page updates, and now the page
+		# "1: Player Character 1" as an item in the participant list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		
+		table = self.browser.find_element_by_id('id_participant_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Player Character 1' for row in rows)
+		)
+		
+		# There is still a text box inviting her to add another participant
+		# She enters, "Player Character 2"
 
 		# Auto fail
 		self.fail('Finish the test!')
