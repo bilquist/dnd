38daa1d8d72aae72a328f59ed592a9f64ae3e85c
bilquist <bbilquist@gmail.com>
2019-04-06 08:34:46 -0700

    javascript tests now included with duplication error message dropping on focus or keystroke

--- initiative/static/initiative.js
+++ initiative/static/initiative.js
@@ -1,5 +1,13 @@
-var initialize = function() {
+window.Dnd = {};
+
+window.Dnd.initialize = function() {
 	$('input[name="name"]').on('keypress', function() {
+		$('.has-error').hide();
+	});
+};
+
+window.Dnd.initialize_focus = function() {
+	$('input[name="name"]').on('focus', function() {
 		$('.has-error').hide();
 	});
 };
