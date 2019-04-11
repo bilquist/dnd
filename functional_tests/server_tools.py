37e09227cd7accfb4768a28c21ff853c5a28d5e5
bilquist <bbilquist@gmail.com>
2019-04-11 09:48:17 -0700

    fixed typo in server_tools.py

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -16,7 +16,7 @@ def reset_database(host):
 def create_session_on_server(host, email):
 	manage_dot_py = _get_manage_dot_py(host)
 	with settings(host_string=f'bilquist@{host}'):
-		session_key = run(f'{manage_dot_py} create_session {email}"')
+		session_key = run(f'{manage_dot_py} create_session {email}')
 		return session_key.strip()
 	
 	
